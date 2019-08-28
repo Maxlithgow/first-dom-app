d05b84e7e1d3fe239c2eaea618d2b7fbe8307776
Craig Lithgow <craiglithgow@CraigdeMacBook-Air.local>
2019-08-28 15:46:05 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function onFormSubmit() {
+    // getting the values of the form
+    var form = document.getElementById('my-form');
+    console.log(form);
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value; 
+    console.log(name);
+    console.log(email);
+    var table = document.getElementById("my-table").getElementsByTagName("tbody")[0];
+    console.log(table);
+    var newRow = table.insertRow();
+    let cell = newRow.insertCell(0);
+    cell.innerHTML = name;
+    let cell2 = newRow.insertCell(1);
+    cell2.innerHTML = email;
+
+    // add to the table:
+    // check out documentation to get from bdy and insert a new row with the entry values
+}
+function playSong(){
+    var audio = new Audio('LaCampanella.mp3')
+    audio.play();
+}
